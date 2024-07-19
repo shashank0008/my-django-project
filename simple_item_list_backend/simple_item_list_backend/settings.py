f1f5b4d15c53c21db6d1aedd0bc431dce82039b0
shashank0008 <30978337+shashank0008@users.noreply.github.com>
2024-07-19 16:37:21 -0400

    Update settings.py

--- simple_item_list_backend/simple_item_list_backend/settings.py
+++ simple_item_list_backend/simple_item_list_backend/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'simple_item_list_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
